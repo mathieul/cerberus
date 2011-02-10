b27822784aedbcb62ece6260b3038102dfd6817f
Mathieu Lajugie <mlajugie@liveops.com>
2011-02-10 12:56:07 -0800

    finished basic multi-lock capability

--- app/lib/cerberus.rb
+++ app/lib/cerberus.rb
@@ -1,8 +1,10 @@
+require 'redis/lock'
+
 module Cerberus
   extend self
   attr_reader :redis, :max_concurrent_access
 
-  KEY_NAME_AVAILABLE_LOCKS = "global:list:freelocks"
+  KEY_NAME_FREE_LOCKS = "global:list:freelocks"
   KEY_NAME_USED_LOCKS = "global:list:usedlocks"
 
   def setup(c = {})
@@ -17,17 +19,37 @@ module Cerberus
 
   def max_concurrent_access=(value)
     @redis.multi
-    @redis.del(KEY_NAME_AVAILABLE_LOCKS)
+    @redis.del(KEY_NAME_FREE_LOCKS)
     @redis.del(KEY_NAME_USED_LOCKS)
-    value.times { |i| @redis.lpush(KEY_NAME_AVAILABLE_LOCKS, lock_name(i)) }
+    value.times { |i| @redis.lpush(KEY_NAME_FREE_LOCKS, lock_name(i)) }
     @redis.exec
     @max_concurrent_access = value
   end
 
   def take_lock
-    key_name, lock = @redis.brpop(KEY_NAME_AVAILABLE_LOCKS, 1)
-    @redis.lpush(KEY_NAME_USED_LOCKS, lock)
+    lock = nil
+    glock.lock do
+      key_name, lock = @redis.brpop(KEY_NAME_FREE_LOCKS, 1)
+      @redis.lpush(KEY_NAME_USED_LOCKS, lock)
+    end
     lock
+  end
+
+  def release_lock(lock)
+    glock.lock do
+      @redis.lrem(KEY_NAME_USED_LOCKS, 1, lock)
+      @redis.lpush(KEY_NAME_FREE_LOCKS, lock)
+    end
+    true
+  end
+
+  def lock_status
+    {}.tap do |status|
+      glock.lock do
+        status[:free] = @redis.llen(KEY_NAME_FREE_LOCKS)
+        status[:used] = @redis.llen(KEY_NAME_USED_LOCKS)
+      end
+    end
   end
 
   private
@@ -35,5 +57,9 @@ module Cerberus
   def lock_name(i)
     index = "%03d" % (i + 1)
     "LOCK##{index}"
+  end
+
+  def glock
+    @glock ||= Redis::Lock.new('cerberus_lock', :expiration => 0, :timeout => 0.1)
   end
 end
--- spec/lib/cerberus_spec.rb
+++ spec/lib/cerberus_spec.rb
@@ -24,11 +24,36 @@ describe Cerberus do
   end
 
   describe "limit number of concurrent accesses" do
-    it "can take a lock if there is at least one available" do
+    it "can take a lock if there is at least one available with #take_lock" do
       Cerberus.max_concurrent_access = 3
       Cerberus.take_lock.should == "LOCK#001"
       Cerberus.take_lock.should == "LOCK#002"
       Cerberus.take_lock.should == "LOCK#003"
+    end
+
+    it "return nil if no lock is available within 1 sec with #take_lock" do
+      Cerberus.max_concurrent_access = 1
+      Cerberus.take_lock.should == "LOCK#001"
+      Cerberus.take_lock.should be_nil
+    end
+
+    it "can return a used lock with #release_lock" do
+      Cerberus.max_concurrent_access = 1
+      Cerberus.take_lock.should == "LOCK#001"
+      Cerberus.release_lock("LOCK#001").should be_true
+      Cerberus.take_lock.should == "LOCK#001"
+    end
+
+    it "returns the number of available and used locks with #lock_status" do
+      Cerberus.max_concurrent_access = 3
+      Cerberus.lock_status.should == {:free => 3, :used => 0}
+      Cerberus.take_lock.should == "LOCK#001"
+      Cerberus.lock_status.should == {:free => 2, :used => 1}
+      Cerberus.take_lock.should == "LOCK#002"
+      Cerberus.take_lock.should == "LOCK#003"
+      Cerberus.lock_status.should == {:free => 0, :used => 3}
+      Cerberus.release_lock("LOCK#001")
+      Cerberus.lock_status.should == {:free => 1, :used => 2}
     end
   end
 end
