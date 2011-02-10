bef2fc75046f1a72bc0b1fbbbe8ea14488833775
Mathieu Lajugie <mlajugie@liveops.com>
2011-02-10 10:59:56 -0800

    make autotest work with app/* files

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,3 +1,15 @@
 require "autotest/growl"
+require "autotest/rspec2"
+
+class Autotest::Rspec2
+  alias :orig_setup_rspec_project_mappings :setup_rspec_project_mappings
+
+  def setup_rspec_project_mappings
+    orig_setup_rspec_project_mappings
+    add_mapping(%r%^app/(.*)\.rb$%) { |_, m|
+      ["spec/#{m[1]}_spec.rb"]
+    }
+  end
+end
 
 Autotest.add_discovery { "rspec2" }
